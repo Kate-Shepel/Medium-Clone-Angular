ede4a248ac15c169713997ff9078adcd2a9c3331
Kate-Shepel <kateborichevskaya@gmail.com>
2024-09-24 16:31:20 +0200

    feat: add auth module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,4 @@ import {Component} from '@angular/core'
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  foo() {
-    console.log('foo')
-  }
-}
+export class AppComponent {}
