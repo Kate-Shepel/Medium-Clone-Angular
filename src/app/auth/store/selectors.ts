79422212a951d1564d2f54771488a44aa694564f
Kate-Shepel <kateborichevskaya@gmail.com>
2024-09-25 13:49:40 +0200

    feat: create selectors for auth + disable sign button on isSubmitting process

---
+++ src/app/auth/store/selectors.ts
@@ -0,0 +1,11 @@
+import {createFeature} from '@ngrx/store'
+import {reducers} from './reducers'
+
+export const authFeature = createFeature({
+  name: 'auth',
+  reducer: reducers,
+})
+
+export const authFeatureSelector = authFeature.selectAuthState
+
+export const isSubmittingSelector = authFeature.selectIsSubmitting
