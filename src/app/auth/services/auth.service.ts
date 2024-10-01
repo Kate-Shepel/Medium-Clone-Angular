a232aaa5c9c71c63461b4556bd10bae430ef2617
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-01 22:17:23 +0200

    feat: prepare store for login page

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,16 +4,29 @@ import {CurrentUserInterface} from 'src/app/shared/currentUser.interface'
 import {map, Observable} from 'rxjs'
 import {HttpClient} from '@angular/common/http'
 import {AuthResponseInterface} from '../types/authResponse.interface'
+import {LoginRequestInterface} from '../types/loginRequestInterface'
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user
+  }
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = 'https://conduit.productionready.io/api/users'
 
     return this.http
       .post<AuthResponseInterface>(url, data)
-      .pipe(map((response: AuthResponseInterface) => response.user))
+      .pipe(map(this.getUser))
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = 'https://conduit.productionready.io/api/users/login'
+
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map(this.getUser))
   }
 }
